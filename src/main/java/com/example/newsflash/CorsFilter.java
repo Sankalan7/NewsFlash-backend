81f05b2b8fad7ce3a231f835e2c3ba806a9449d5
Sankalan <sankalan.chanda312@gmail.com>
2023-05-20 00:21:07 +0530

    removed CorsFilter

---
+++ src/main/java/com/example/newsflash/CorsFilter.java
@@ -0,0 +1,37 @@
+package com.example.newsflash;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code (if any)
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code (if any)
+    }
+}
+
