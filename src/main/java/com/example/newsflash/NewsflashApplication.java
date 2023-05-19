486a823698569fbd633f842b5d22a5753cd09f68
Sankalan <sankalan.chanda312@gmail.com>
2023-05-20 01:07:45 +0530

    mistake found

--- src/main/java/com/example/newsflash/NewsflashApplication.java
+++ src/main/java/com/example/newsflash/NewsflashApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.newsflash")
 public class NewsflashApplication {
 
 	public static void main(String[] args) {
