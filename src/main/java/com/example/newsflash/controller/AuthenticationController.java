c0325adf48ccf940725e4293c0a5d5838ff663db
Sankalan <sankalan.chanda312@gmail.com>
2023-05-19 23:37:33 +0530

    added WebMvcConfig

--- src/main/java/com/example/newsflash/controller/AuthenticationController.java
+++ src/main/java/com/example/newsflash/controller/AuthenticationController.java
@@ -8,7 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
